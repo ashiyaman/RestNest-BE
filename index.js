a58c8c7eb286f9d494d3d62bf24e94fc9388d54a
ashiyaman <ashiya1005@gmail.com>
2025-02-13 05:35:36 +0530

    fix: Add config files

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ app.delete('/hotels/:hotelId', async (req, res) => {
     }
 })
 
-const PORT = 3000
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log('Server running on port', PORT)
 })
